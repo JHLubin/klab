44a1b9282878afaa461b50328d483ff48752d255
JHLubin <jhlubin1@gmail.com>
2020-01-29 20:40:21 -0500

    Fixed but in residue_interface_intE.py that it kept reading a reference PDB, which it didn't need to have in the first place.

--- fibrils_collaboration/residue_interface_intE.py
+++ fibrils_collaboration/residue_interface_intE.py
@@ -12,8 +12,6 @@ def parse_args():
 	parser = argparse.ArgumentParser(description=info)
 	parser.add_argument("-d", "--directory", required=True,
 		help="Pick a folder to analyze")
-	parser.add_argument("-ref", "--reference", required=True,
-		help="Pick a PDB file to compare against")
 	args = parser.parse_args()
 	return args
 
@@ -30,7 +28,6 @@ init()
 pdbs = glob(join(args.directory, '*.pdb'))
 pdbs += glob(join(args.directory, '*.pdb.gz'))
 pdbs.sort()
-pose = pose_from_pdb(args.reference)
 sf = get_fa_scorefxn()
 tem = TotalEnergyMetric()
 tem.set_scorefunction(sf)
@@ -51,11 +48,11 @@ with open(join(args.directory, report_name), 'w') as w:
 		this_res = [p, tem.calculate(pp)]
 		for i in range(1, chain_1_size + 1):
 			print(i)
-			e = calc_residue_interaction(pose, sf, i, csb)
+			e = calc_residue_interaction(pp, sf, i, csb)
 			this_res.append(e)
 		print('chain change')
 		for i in range(chain_1_size + 1, chain_1_size + chain_2_size + 1):
 			print(i)
-			e = calc_residue_interaction(pose, sf, i, csa)
+			e = calc_residue_interaction(pp, sf, i, csa)
 			this_res.append(e)
 		w.write(','.join([str(x) for x in this_res])+'\n')
